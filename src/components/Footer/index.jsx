bb9ce9536e8c32e77966bcdd9d2ce119c344beee
Baryova <baryova42@gmail.com>
2023-11-12 22:24:07 +0700

    first commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./style.module.css";
+
+const Footer = () => {
+    return (
+        <footer className={styles.container}>
+            <small>&copy; 2023 | Bukapedia</small>
+        </footer>
+    );
+};
+
+export default Footer;
